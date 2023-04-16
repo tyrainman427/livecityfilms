805c2576ac5f1a2a40a17a23c3ebdd03e0b20d93
tyrainman427 <tyraineytech@gmail.com>
2023-04-16 14:26:43 -0400

    add stripe payments

--- livecity/films/admin.py
+++ livecity/films/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+class PriceInlineAdmin(admin.TabularInline):
+    model = OrderDetail
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PriceInlineAdmin]
+
+
+admin.site.register(Product, ProductAdmin)
+
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(OrderDetail)
